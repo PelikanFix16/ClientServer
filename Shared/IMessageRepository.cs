de8f4345105de55e902fd3447484296fb0b2c5fe
PelikanFix16 <hubertosypowicz@outlook.com>
2021-10-28 21:43:57 +0200

    Repository pattern for handling save to database

---
+++ Shared/IMessageRepository.cs
@@ -0,0 +1,13 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shared
+{
+    public interface IMessageRepository
+    {
+        public void Add(string message);
+
+    }
+}
---
+++ Shared/MessageDatabaseRepository.cs
@@ -0,0 +1,30 @@
+﻿using Configurations;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shared
+{
+    public class MessageDatabaseRepository : IMessageRepository
+    {
+        private readonly MessageContext messageContext;
+
+        public MessageDatabaseRepository()
+        {
+            messageContext = new MessageContext();
+
+
+        }
+
+        public void Add(string message)
+        {
+            Message msg = new Message() { Msg = message, Time = DateTime.Now };
+            messageContext.Messages.Add(msg);
+            messageContext.SaveChanges();
+
+
+
+        }
+    }
+}
