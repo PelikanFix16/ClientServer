469e6b6b55d7979de9e83be25248a8abfad2f60f
PelikanFix16 <hubertosypowicz@outlook.com>
2021-10-28 22:06:52 +0200

    Write to file repository

--- Shared/MessageFileRepository.cs
+++ Shared/MessageFileRepository.cs
@@ -1,11 +1,30 @@
-﻿using System;
+﻿using Configurations;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace Shared
 {
-    public class MessageFileRepository
+    public class MessageFileRepository : IMessageRepository
     {
+        private readonly Config config;
+        public MessageFileRepository()
+        {
+            config = ConfigCreate.Create();
 
+        }
+
+        public void Add(string message)
+        {
+            string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, config.File);
+            using (FileStream fs = new FileStream(path,FileMode.Append,FileAccess.Write))      
+                using (TextWriter file = new StreamWriter(fs))
+                {
+                    file.WriteLine(String.Format("Message: {0} Time: {1}", message, DateTime.Now));
+
+                }
+            
+        }
     }
 }
