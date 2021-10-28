39b898c7969091930f3510979df6d8122025b6f6
PelikanFix16 <hubertosypowicz@outlook.com>
2021-10-28 21:43:01 +0200

    Database migrations and database context

---
+++ Models/Message.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Models
+{
+
+    public class Message
+    {
+
+        public int Id { get; set; }
+        public string Msg { get; set; }
+        public DateTime Time { get; set; }
+        
+
+    }
+}
---
+++ Models/MessageContext.cs
@@ -0,0 +1,33 @@
+﻿using Configurations;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models
+{
+    public class MessageContext: DbContext
+    {
+        public DbSet<Message> Messages { get; set; }
+        private readonly Config config;
+
+
+        public MessageContext()
+        {
+            config = ConfigCreate.Create();
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            if (!options.IsConfigured)
+            {
+                options.UseSqlServer(config.DefaultDB);
+
+            }
+            base.OnConfiguring(options);
+
+        }
+
+    }
+}
